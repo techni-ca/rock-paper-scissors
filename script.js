70697bfcdefe911c62a4d508bdb32f8dea3cb09d
Andrew <48330373+techni-ca@users.noreply.github.com>
2023-02-21 16:07:29 -0700

    Make UI elements work

--- script.js
+++ script.js
@@ -6,9 +6,6 @@ function getComputerChoice() {
         case 2: return "Scissors"; 
     }
 }
-function capitalise(inString) {
-    return inString.slice(0,1).toUpperCase() + inString.slice(1).toLowerCase();
-}
 function playRound(playerChoice,computerChoice) {
     if (playerChoice == computerChoice) {
         return `You draw!  You both chose "${playerChoice}"`;
@@ -16,25 +13,47 @@ function playRound(playerChoice,computerChoice) {
     if ((playerChoice == "Rock" && computerChoice == "Scissors") ||
         (playerChoice == "Paper" && computerChoice == "Rock") ||
         (playerChoice == "Scissors" && computerChoice == "Paper")) {
+        score1++;
         return `You win! ${playerChoice} beats ${computerChoice}`;
     }
+    score2++;
     return `You lose!  ${computerChoice} beats ${playerChoice}`;
 }
-function playFunction(choice) {
-    choice = capitalise(choice);
-    switch (choice) {
-        case "Rock":
-        case "Paper":
-        case "Scissors":
-            return playRound(choice,getComputerChoice());
-            break;
-        default:
-            return `${choice} is not a valid move to play!`;
-    }
-}
-const buttons = document.querySelectorAll('#rock,#paper,#scissors');
+const buttons = document.querySelectorAll('#Rock,#Paper,#Scissors');
+const result = document.querySelector("#Results");
+const playerScore = document.querySelector("#PlayerScore");
+const computerScore = document.querySelector("#ComputerScore");
+let score1 = 0;
+let score2 = 0;
+let clearOnNextUpdate = true;
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(playFunction(button.id));
+        if (clearOnNextUpdate) {
+            const elements=document.querySelectorAll('#Results p');
+            elements.forEach((element) => {
+                result.removeChild(element);
+            });
+            clearOnNextUpdate = false;
+        }
+        const p = document.createElement('p');
+        p.textContent = playRound(button.id,getComputerChoice());
+        result.appendChild(p);
+        playerScore.textContent = score1;
+        computerScore.textContent = score2;
+        if (score1 == 5 || score2 == 5) {
+            const finalP = document.createElement('p');
+            if (score1 == 5) {
+                finalP.textContent = "You were first to five!  You win overall!";
+            } else {
+                finalP.textContent = "Computer was first to five!  You lose overall!"
+            }
+            score1 = 0;
+            score2 = 0;
+            result.appendChild(finalP);
+            clearOnNextUpdate = true;
+        }
     })
 });
+
+playerScore.textContent = "0";
+computerScore.textContent = "0";
